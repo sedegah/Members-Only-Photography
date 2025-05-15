9bbecd486fecf3919d2e3e6da0d92b652a47effa
Kimathi Sedegah <sedkim370@gmail.com>
2025-05-15 18:12:24 +0100

    Update Footer.tsx

--- src/components/navigation/Footer.tsx
+++ src/components/navigation/Footer.tsx
@@ -20,81 +20,26 @@ const Footer = () => {
                   crossOrigin="anonymous"
                 />
               </div>
-              <h3 className="text-xl font-medium mt-2">Members Only Photography</h3>
+              <h3 className="text-xl font-medium mt-2">
+                Members Only Photography
+              </h3>
             </div>
 
             <div className="flex flex-col md:items-end gap-4">
-              <div className="flex items-center gap-2">
-                <Image
-                  src="https://ext.same-assets.com/1974940630/4252861238.svg"
-                  alt="Location Icon"
-                  width={24}
-                  height={24}
-                  crossOrigin="anonymous"
-                />
-                <span>GM 043 8877</span>
-              </div>
-
-              <div className="flex items-center gap-2">
-                <Image
-                  src="https://ext.same-assets.com/1974940630/3301625649.svg"
-                  alt="Phone Icon"
-                  width={24}
-                  height={24}
-                  crossOrigin="anonymous"
-                />
-                <Link href="tel:+233551307929" className="hover:underline">
-                  +233551307929
-                </Link>
-              </div>
-
-              <div className="flex items-center gap-2">
-                <Image
-                  src="https://ext.same-assets.com/1974940630/2067545548.svg"
-                  alt="Email Icon"
-                  width={24}
-                  height={24}
-                  crossOrigin="anonymous"
-                />
-                <Link href="mailto:sedkim370@gmail.com" className="hover:underline">
-                  sedkim370@gmail.com
-                </Link>
-              </div>
+              <span>GM 043 8877</span>
+              <Link href="tel:+233551307929" className="hover:underline">
+                +233551307929
+              </Link>
+              <Link href="mailto:sedkim370@gmail.com" className="hover:underline">
+                sedkim370@gmail.com
+              </Link>
             </div>
           </div>
 
           <div className="mt-8 border-t border-gray-800 pt-8">
-            <p className="text-sm md:text-base">Showing our audience the world through the lens of a camera</p>
-
-            <div className="flex items-center gap-4 mt-4">
-              <Link href="https://twitter.com/heiskimathii" target="_blank" rel="noopener noreferrer">
-                <Image
-                  src="https://ext.same-assets.com/1974940630/2320907649.svg"
-                  alt="Twitter"
-                  width={20}
-                  height={20}
-                  crossOrigin="anonymous"
-                />
-              </Link>
-              <Link href="https://www.instagram.com/kim.ath.i" target="_blank" rel="noopener noreferrer">
-                <Image
-                  src="https://ext.same-assets.com/1974940630/1998606599.svg"
-                  alt="Instagram"
-                  width={20}
-                  height={20}
-                  crossOrigin="anonymous"
-                />
-              </Link>
-              <Link href="https://snapchat.com/add/mr.kimathi" target="_blank" rel="noopener noreferrer">
-                <Image
-                  src="https://ext.same-assets.com/1974940630/1367661487.svg"
-                  alt="Snapchat"
-                  width={20}
-                  height={20}
-                  crossOrigin="anonymous"
-                />
-              </Link>
-            </div>
+            <p className="text-sm md:text-base">
+              Showing our audience the world through the lens of a camera
+            </p>
           </div>
         </div>
       </div>
@@ -102,12 +47,4 @@ const Footer = () => {
       <div className="bg-pink-light py-4 text-center">
         <div className="container mx-auto px-4">
           <p className="text-sm">
-            © 2023-2025 Members Only Photography. All Rights Reserved.
-          </p>
-        </div>
-      </div>
-    </footer>
-  );
-};
-
-export default Footer;
+            © 2023-2025 Members Only Photography.
